879b3162183178f8da0c84e9e695b42f1997fbf1
Yashmin <yashmin.sainju@selu.edu>
2018-04-08 18:23:32 -0500

    Basic setup ready, navBar setup, carousel setup to do

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { IntroComponent } from './carousel/intro/intro.component';
+import { FormComponent } from './carousel/intro/form/form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CarouselComponent,
+    IntroComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
